2e40f0213e79a9b486aeb067b9284cfd42738f8c
Jordevá <jordevalucas@gmail.com>
2021-04-13 21:33:42 -0300

    #3: configura arquivo de rotas

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import 'reflect-metadata'
 import express from "express";
+import { router } from './routes';
 import "./database"
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" })
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"))
