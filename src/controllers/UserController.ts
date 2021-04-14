d022475edeb3b4ce26d9e09bc78ab1ef0c569bd7
Jordevá <jordevalucas@gmail.com>
2021-04-14 20:32:58 -0300

    #3: adiciona controller de usuário

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../models/User";
+
+class UserController {
+}
+
+export { UserController }
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import { UserController } from './controllers/UserController';
 
 const router = Router();
+
+const userController = new UserController();
 
 
 export { router };
