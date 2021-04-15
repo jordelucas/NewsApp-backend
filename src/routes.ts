6641e070bb0fc79c70a669cfba71d5a2dac3b4bc
Jordevá <jordevalucas@gmail.com>
2021-04-14 21:45:37 -0300

    #5: adiciona controller de convite

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { UserController } from './controllers/UserController';
+import { InvitationController } from './controllers/InvitationController';
 
 const router = Router();
 
 const userController = new UserController();
+const invitationController = new InvitationController();
 
 router.post('/users', userController.create);
 
