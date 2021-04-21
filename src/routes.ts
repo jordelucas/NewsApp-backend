3448fa84304873284dad47941b07c0419b7de717
Jordevá <jordevalucas@gmail.com>
2021-04-20 22:10:33 -0300

    #11: cria rota de cadastro de notifica

--- src/routes.ts
+++ src/routes.ts
@@ -2,16 +2,19 @@ import { Router } from 'express';
 import { UserController } from './controllers/UserController';
 import { InvitationController } from './controllers/InvitationController';
 import { AuthenticationController } from './controllers/AuthenticationController';
+import { NewsController } from './controllers/NewsController';
 
 const router = Router();
 
 const userController = new UserController();
 const invitationController = new InvitationController();
-const authencicationController = new AuthenticationController();
+const authenticationController = new AuthenticationController();
+const newsController = new NewsController();
 
 router.post('/users', userController.create);
 router.post('/invitation', invitationController.create);
 router.get('/invitation/:id', invitationController.validade);
-router.post('/authentication', authencicationController.authenticate);
+router.post('/authentication', authenticationController.authenticate);
+router.post('/news', newsController.create);
 
 export { router };
